096d5ecfb708f93ad21c24d547116935e9fe5961
Daniel Gallego <dangallego3@gmail.com>
2024-02-05 12:01:44 -0500

    neatens up file for better readability

--- 2-1-Kinematics-Ball-Drop.py
+++ 2-1-Kinematics-Ball-Drop.py
@@ -7,22 +7,19 @@ Use your program to calculate the time for a ball dropped from a 100 m high towe
 import numpy as np
 import sys
 
-g = 10 
-# if using argv - no need to input 
-#h = float(input("Enter the height of the tower that the ball is being dropped from (in meters): \n"))
-#h = float(sys.argv[1])
+g = 10 # setting gravity to 10 (engineering route) 
 
-if len(sys.argv) != 1: #checks if length of arguments returned is not 1 (in which case we entered the height in the terminal after the file)
-    h = float(sys.argv[1])
+# if using argv - no need to input
+if len(sys.argv) != 1: # checks if length of arguments returned is not 1 (in which case we entered the height in the terminal after the file)
+    h = float(sys.argv[1]) # user enters height in terminal in same line after running .py file
 else: #if we just run the file, this condition prompts us to enter the distance the ball is dropped
-    h = float(input("Enter the height of the tower that the ball is being dropped from (in meters): \n"))
+    h = float(input("Enter the height of the tower that the ball is being dropped from (in meters): \n")) # user enter height after running .py file
 
 #if type(h) != float or type(h) != int: ### THIS WON'T WORK bc type is always originally string (then converted) --> argparse works better here
 #    print("You must type a number") ### issues here actually arise from not being able to convert (ie. user typing '75 meters' instead of '75')
 
 t = np.sqrt((2*h)/(g)) #equation to find time 
 
-#print("The total time the ball takes to hit the ground is: ", t, " seconds")
 text = "The total time the ball takes to hit the ground is {:.2f} seconds!".format(t) # formats text to print time to only show last two digits 
 print(text, "\n", sys.argv, "length:", len(sys.argv))
 

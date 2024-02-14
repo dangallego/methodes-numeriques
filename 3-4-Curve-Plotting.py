33f2d4c9b926ae93c63bf1a7f861a7605b3f1650
Daniel Gallego <dangallego3@gmail.com>
2024-02-13 19:28:02 -0500

    updates curve plotting file with parsing functionality

--- 3-4-Curve-Plotting.py
+++ 3-4-Curve-Plotting.py
@@ -17,13 +17,6 @@ def plot(x,y, alpha = 1):
     plt.xlabel('x') ; plt.ylabel('y')
     plt.show()
 
-def polarplot(r,theta):
-    '''Polar representation of values.'''
-    fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
-    ax.plot(theta, r)
-    ax.grid(True)
-    plt.show()
-
 def deltoid_curve(num_theta_vals=100):
     '''Takes a set of values of theta between zero and 2pi and calculates / plots x and y.''' 
     theta_vals = np.linspace(0, 2*np.pi, num_theta_vals) 
@@ -42,9 +35,9 @@ def Galilean_spiral(num_theta_vals=100):
     # polar coordinates
     r = theta_vals**2 
     x = r*cos(theta_vals) ; y = r*sin(theta_vals)
-    return x, y, r, theta_vals
+    return x, y
 
-x,y,r,thetas = Galilean_spiral(10000)
+x,y = Galilean_spiral(10000)
 #plot(x,y)
 
 # POLAR REPRESENTATION
@@ -58,7 +51,67 @@ def Feys_funct(num_theta_vals=100):
     # polar coordinates
     r = np.exp(cos(theta_vals)) - 2*cos(4*theta_vals) + (sin(theta_vals/12))**5
     x = r*cos(theta_vals) ; y = r*sin(theta_vals)
-    return x, y, r, theta_vals
+    return x, y
 
-x,y,r,thetas = Feys_funct(1000)
-plot(x,y, 0.8)
+x,y = Feys_funct(1000)
+#plot(x,y, 0.8)
+
+# Create subplot example 
+N = 1000
+x1,y1 = deltoid_curve(N)
+x2,y2 = Galilean_spiral(N)
+x3,y3 = Feys_funct(N)
+
+
+def plot_3(N=1000):
+    '''Plots all three plots next to each other. '''
+    x1,y1 = deltoid_curve(N)
+    x2,y2 = Galilean_spiral(N)
+    x3,y3 = Feys_funct(N)
+    # makes the plot 
+    figure, axs = plt.subplots(1,3, figsize= (14,4))
+    axs[0].plot(x1,y1, color = 'darkslategrey', lw = 5)
+    axs[0].set_title('Deltoid curve')
+    axs[0].set_ylabel('y')
+    axs[1].plot(x2,y2, color = 'darkslategrey', lw = 5)
+    axs[1].set_title('Galilean spiral')
+    axs[1].set_xlabel('x')
+    axs[2].plot(x3,y3, color = 'darkslategrey', lw = 2)
+    axs[2].set_title('Fey\'s function')
+    plt.show()
+
+#plot_3()
+
+# Parser section
+parser = argparse.ArgumentParser(
+                    prog='Curve-Plotting',
+                    description='Shows plot(s) of Polar function in Cartesian coordinates.',
+                    epilog='Text at the bottom of help')
+# easiest to make -graph optional argument, default is to all 
+parser.add_argument('-g', '--graph', choices = ['deltoid', 'galilean', 'feys', 'all', 
+                                         'Deltoid', 'Galilean', 'Feys', 'All'], default = 'all', # default is to plot all graphs
+                    action = 'store', help='Must select one of the possible graph options.') # added helpful help message
+parser.add_argument('-n', '--ntheta', type = int, # optional argument 
+                    action = 'store', default = 1000, # default of 1000 points to generate curves
+                    help='Optional argument, choose number of points to plot function') 
+
+args = parser.parse_args()
+print(args)
+
+if args.graph == 'deltoid' or args.graph =='Deltoid':
+    x,y = deltoid_curve(args.ntheta)
+    plot(x,y)
+    plt.title('Deltoid curve')
+elif args.graph == 'galilean' or args.graph == 'Galilean':
+    x,y = Galilean_spiral(args.ntheta)
+    plot(x,y)
+    plt.title('Galilean spiral')
+elif args.graph == 'feys' or args.graph == 'Feys':
+    x,y = Feys_funct(args.ntheta)
+    plot(x,y)
+    plt.title('Fey\'s function')
+elif args.graph == 'all' or args.graph == 'All':
+    plot_3(args.ntheta)
+else:
+    plot_3(args.ntheta)
+
